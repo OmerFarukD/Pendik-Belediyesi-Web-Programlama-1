8a43cad36077814ba44aa51ca18ce52d7d24b1ae
Pendik <Pendik@BIR>
2024-09-13 14:49:10 +0300

    Lambda lı gösterim

---
+++ Encapsulation/Book.cs
@@ -0,0 +1,29 @@
+﻿namespace Encapsulation;
+// kitap -> Başlığı , Açıklaması , Yazar Adı, Yazar Soyadı,
+// Yayın evi ,Basım Tarihi ,
+//Yazar Tam adı,  (Yayın evi - Basım Tarihi)
+public class Book
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+
+    public string AuthorName { get; set; }
+
+    public string AuthorSurname { get; set; }
+
+    public string Publisher { get; set; }
+
+    public string PublishDate { get; set; }
+
+    public string AuthorFullName => $"{AuthorName} {AuthorSurname}";
+
+    public string PublishInfo => $"{Publisher} - {PublishDate}";
+
+    public override string ToString()
+    {
+        return $"Başlık : {Title}, Açıklama : {Description}" +
+            $"Yazar Adı: {AuthorName}, Yazar Soyadı: {AuthorSurname}" +
+            $"Yayın Evi : {Publisher}, Basım Tarihi : {PublishDate}" +
+            $"Yayın Evi info : {PublishInfo}, Yazar Full adı : {AuthorFullName}";
+    }
+}
--- Encapsulation/Program.cs
+++ Encapsulation/Program.cs
@@ -9,10 +9,25 @@ User user = new User()
     LastName = "Doğan",
     Email = "omerdogan_cezaevi@gmail.com",
     Password = "123456",
-    UserName = "qubitfaruk",
-   
+    UserName = "qubitfaruk"
 };
+
+Book book = new Book()
+{
+    AuthorName = "Sait Faik",
+    AuthorSurname= "Abası Yanık",
+    Description = "En sevdiğim harikaa Sütlacı çok severim arkadaşlar.",
+    PublishDate = "25 Ocak 2004",
+    Publisher = "ISKA yayın evi",
+    Title = "ISKA da Trajikomik olaylar."
+};
+
+Console.WriteLine(book);
+
 
 Console.WriteLine(user.LastName);
 
 
+// kitap -> Başlığı , Açıklaması , Yazar Adı, Yazar Soyadı,
+// Yayın evi ,Basım Tarihi ,
+//Yazar Tam adı,  (Yayın evi - Basım Tarihi)
